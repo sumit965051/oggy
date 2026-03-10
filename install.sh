fe63361c511e8551892d3b60559c56d5c2d97f7e
sumit965051 <sumitkumar989931@gmail.com>
2026-03-10 12:54:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,24 +14,24 @@ echo -e "${GREEN}        INSTALLING JATIIN SIINGH TOOL           ${NC}"
 echo -e "${CYAN}==========================================${NC}"
 echo ""
 
-# Check if 'jack' file exists
-if [ ! -f "jack" ]; then
-  echo -e "${RED}[✘] Error: 'jack' file not found in current directory!${NC}"
+# Check if 'Kunal' file exists
+if [ ! -f "Kunal" ]; then
+  echo -e "${RED}[✘] Error: 'Kunal' file not found in current directory!${NC}"
   exit 1
 fi
 
 echo -e "${YELLOW}[*] Setting permissions...${NC}"
 sleep 1
-chmod +x jack
+chmod +x Kunal
 
 echo -e "${YELLOW}[*] Moving to system bin...${NC}"
 sleep 1
 
 if [ -d "$PREFIX/bin" ]; then
-  mv -f jack $PREFIX/bin/
+  mv -f Kunal $PREFIX/bin/
   INSTALL_PATH="$PREFIX/bin"
 elif [ -d "/usr/local/bin" ]; then
-  mv -f jack /usr/local/bin/
+  mv -f Kunal /usr/local/bin/
   INSTALL_PATH="/usr/local/bin"
 else
   echo -e "${RED}[✘] Error: Installation path not found!${NC}"
@@ -44,5 +44,5 @@ echo -e "${CYAN}------------------------------------------${NC}"
 echo -e "${GREEN}      INSTALLATION COMPLETE BITXXH ✅           ${NC}"
 echo -e "${CYAN}------------------------------------------${NC}"
 echo -e "${YELLOW}Now you can run the tool by Dfg x Ncf:${NC}"
-echo -e "${BLUE}>> jack${NC}"
+echo -e "${BLUE}>> Kunal${NC}"
 echo ""
